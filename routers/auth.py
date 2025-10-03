208ad4bb513e2e160290778c2604889c1268ec04
JAFARY <jaffarymdegela@gmail.com>
2025-10-03 10:41:29 +0300

    working-hashedpassword

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,12 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 from models import Users
+from passlib.context import CryptContext
+
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
 
 class CreateUserRequest(BaseModel):
     username:str
@@ -17,7 +21,7 @@ class CreateUserRequest(BaseModel):
 async def create_user(create_user_request:CreateUserRequest):
     create_user_model = Users(
         email = create_user_request.email,
-        hashed_password = create_user_request.password,
+        hashed_password = bcrypt_context.hash(create_user_request.password),
         username = create_user_request.username,
         first_name = create_user_request.first_name,
         last_name = create_user_request.last_name,
